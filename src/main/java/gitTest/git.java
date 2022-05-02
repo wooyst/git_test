573a13d3a7a2c8a62be16dd43ba799e60c894de0
wooy <m1669271045@163.com>
2022-05-02 19:47:45 +0800

    third commit

--- src/main/java/gitTest/git.java
+++ src/main/java/gitTest/git.java
@@ -8,5 +8,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello github");
+        System.out.println("hello gitee");
     }
 }
