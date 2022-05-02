28ac1597264d9b8133fdd89c01d01dc18a7e865a
wooy <m1669271045@163.com>
2022-05-02 21:52:14 +0800

    main conflict commit

--- src/main/java/gitTest/git.java
+++ src/main/java/gitTest/git.java
@@ -10,5 +10,6 @@ public class git {
         System.out.println("hello github");
         System.out.println("hello gitee");
         System.out.println("hello hot-fix");
+        System.out.println("hello main conflict");
     }
 }
