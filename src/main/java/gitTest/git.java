cf6224dd9f0b71ff6ac621b24bf224b605726015
wooy <m1669271045@163.com>
2022-05-02 19:40:15 +0800

    first commit

---
+++ src/main/java/gitTest/git.java
@@ -0,0 +1,11 @@
+package gitTest;
+
+/**
+ * @author wooy
+ * @create 2022 - 05 - 2022/5/2 - 19:25
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
