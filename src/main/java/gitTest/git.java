34c3735351d4f683e216932883f53d6141109b14
wooy <m1669271045@163.com>
2022-05-02 20:50:35 +0800

    hot-fix first commit

--- src/main/java/gitTest/git.java
+++ src/main/java/gitTest/git.java
@@ -9,5 +9,6 @@ public class git {
         System.out.println("hello git");
         System.out.println("hello github");
         System.out.println("hello gitee");
+        System.out.println("hello hot-fix");
     }
 }
